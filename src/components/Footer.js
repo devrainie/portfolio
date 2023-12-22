2297a05d95c833ceda02888432e861dbdf607a12
rainxx3 <rainietamakloe@gmail.com>
2023-12-22 23:18:07 +0000

    adding image and text for landing page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { CardFooter } from "react-bootstrap";
+
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <CardFooter></CardFooter>
+        </div>
+        
+    )
+};
---
+++ src/components/page-sections/Landing.js
@@ -0,0 +1,59 @@
+import HeroImg from '../../assets/images/hero-img1.png';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import Typed from 'react-typed';
+import './Landing.css';
+
+
+export default function Landing(){
+    return(
+        <div>
+            <Container>
+                <Row>
+                    <Col className='col-style'>
+                        <Typed 
+                        className='typed-text'
+                        strings={[
+                            "Hi, my name is ",
+                            "I am an aspiring full stack developer",
+                            "I love coding creative projects"
+                        ]}
+                        typeSpeed={50}
+                        backSpeed={30}
+                        loopCount={5}
+                        />
+                        <p id='hero-txt'>I am currently a junior cloud and dev-ops engineer who also enjoys web development. Recently, i completed the Code First Girls 16 week boot camp on the full stack stream. Scroll to learn more about my journey!</p>
+
+                        <Row>
+                            <Col className='sm-icons'>
+                                <button className='sm-btn'><FontAwesomeIcon icon={faInstagram} size="2x"/> </button>
+                                {/* <FontAwesomeIcon icon={faInstagram} size="2x"/> */}
+                            </Col>
+                            <Col className='sm-icons'>
+                                <button className='sm-btn'>
+                                    <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+                                </button>
+                                
+                            </Col>
+                            <Col className='sm-icons'>
+                                <button className='sm-btn'>
+                                    <FontAwesomeIcon icon={faGithub} size="2x"/>
+                                </button>
+                                
+                            </Col>
+                        </Row>
+
+                    </Col>
+                    <Col>
+                        <Image src={HeroImg} alt="Hero Image" fluid />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
